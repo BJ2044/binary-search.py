fb4f8e3af43bc4b8f3e0c5be8eb2ad9684fb45bc
Brad Jones <BJ2044@gmail.com>
2022-11-16 10:21:15 -0500

    Binary Search

---
+++ binary-search.py
@@ -0,0 +1,16 @@
+# Binary search
+from typing import List
+def search(nums: List[int], target: int) -> int:
+    length = len (nums) - 1
+    low, high - 0, length
+
+    while low <= high:
+      mid = (low + high) // 2
+      
+      if nums[mid] == target:
+        return mid
+      elif nums[mid] < target:
+        high = mid -1
+      else:
+        low = mid + 1
+    return -1
